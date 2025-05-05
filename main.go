d3f013c9c439d617908fa230caa32b0beea9d332
yaswanth-vakkala <yaswanthv87@gmail.com>
2025-05-05 11:27:45 +0530

    add 2 more routes and related functions

--- main.go
+++ main.go
@@ -9,9 +9,19 @@ func home(w http.ResponseWriter, r *http.Request){
 	w.Write([]byte("Hello from yankbin"))
 }
 
+func yankView(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("display a specific yank"))
+}
+
+func yankCreate(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("display a form for creating an yank"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/yank/view", yankView)
+	mux.HandleFunc("/yank/create", yankCreate)
 
 	log.Print("starting server on :4000")
 

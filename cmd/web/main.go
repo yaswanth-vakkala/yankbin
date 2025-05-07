64006feb74c64fe933d6aca6575b4bdb8758d5e8
yaswanth-vakkala <yaswanthv87@gmail.com>
2025-05-07 11:46:19 +0530

    add static file, css, js

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,8 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("GET /static/", http.StripPrefix("/static",fileServer))
 	mux.HandleFunc("GET	/{$}", home)
 	mux.HandleFunc("GET	/yank/view/{id}", yankView)
 	mux.HandleFunc("GET	/yank/create", yankCreate)

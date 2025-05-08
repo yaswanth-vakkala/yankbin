317f1dad288b973e7be40513dc6ae9ae53a78ab6
yaswanth-vakkala <yaswanthv87@gmail.com>
2025-05-08 12:10:09 +0530

    seperate routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -21,17 +21,9 @@ func main() {
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static",fileServer))
-	mux.HandleFunc("GET	/{$}",app.home)
-	mux.HandleFunc("GET	/yank/view/{id}", app.yankView)
-	mux.HandleFunc("GET	/yank/create", app.yankCreate)
-	mux.HandleFunc("POST /yank/create", app.yankCreateItem)
-
 	logger.Info("starting server", "addr", *addr)
 	
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
